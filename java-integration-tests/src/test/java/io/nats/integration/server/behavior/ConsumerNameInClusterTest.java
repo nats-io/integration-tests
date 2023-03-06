c9669df31be21a1087d07dd6c48bcec1361e1c61
scottf <scottfauerbach@gmail.com>
2023-03-06 17:17:28 -0500

    cluster test will have to wait

--- java-integration-tests/src/test/java/io/nats/integration/server/behavior/ConsumerNameInClusterTest.java
+++ java-integration-tests/src/test/java/io/nats/integration/server/behavior/ConsumerNameInClusterTest.java
@@ -13,39 +13,44 @@ import io.nats.integration.utils.JetStreamTestHelper;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ConsumerNameInClusterTest extends JetStreamTestBase {
 
     @Test
     public void testSingleServer() throws Exception {
-        runInJsServer(nc -> {
-            _testCluster(nc, "conSingle");
-        });
+        runInJsServer(nc -> _testCluster(Collections.singletonList(nc)));
     }
 
-    @Test
-    public void testCluster() throws Exception {
-        runInJsSimpleCluster(runners -> {
-            try (Connection nc = standardConnection(runners.get(0).getURI())) {
-                _testCluster(nc, "conCluster");
-            }
-        });
-    }
+//    @Test
+//    public void testCluster() throws Exception {
+//        runInJsSimpleCluster(ConsumerNameInClusterTest::_testCluster);
+//    }
 
-    private static void _testCluster(Connection nc, String conName) throws IOException, JetStreamApiException {
-        JetStreamTestHelper h = new JetStreamTestHelper(nc);
+    public static void _testCluster(List<Connection> ncs) throws IOException, JetStreamApiException {
+        JetStreamTestHelper h = new JetStreamTestHelper(ncs.get(0));
         createTestMemoryStream(h);
 
-        PullSubscribeOptions subOpts = ConsumerConfiguration.builder()
-            .name(conName)
-            .inactiveThreshold(100000)
-            .buildPullSubscribeOptions();
+        String[] conNames = new String[ncs.size()];
+        JetStreamSubscription[] subs = new JetStreamSubscription[ncs.size()];
+        for (int x = 0; x < ncs.size(); x++) {
+            conNames[x] = uniqueEnough();
+            PullSubscribeOptions subOpts = ConsumerConfiguration.builder()
+                .name(conNames[x])
+                .inactiveThreshold(100000)
+                .buildPullSubscribeOptions();
+            subs[x] = ncs.get(x).jetStream().subscribe(h.subject, subOpts);
+        }
 
-        JetStreamSubscription sub = h.js.subscribe(h.subject, subOpts);
         List<String> consumerNames = h.jsm.getConsumerNames(h.streamName);
-        assertTrue(consumerNames.contains(conName));
+        assertEquals(ncs.size(), consumerNames.size());
+        for (int x = 0; x < ncs.size(); x++) {
+            assertTrue(consumerNames.contains(conNames[x]));
+            subs[x].unsubscribe();
+        }
     }
 }
